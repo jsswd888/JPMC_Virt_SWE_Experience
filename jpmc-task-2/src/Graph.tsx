301aa6e139f40e497a56b6c9535e0affea8aff3c
jsswd888 <ShikwKevin@gmail.com>
2022-12-27 13:58:50 -0600

    fix minor changes

--- jpmc-task-2/src/Graph.tsx
+++ jpmc-task-2/src/Graph.tsx
@@ -56,7 +56,7 @@ class Graph extends Component<IProps, {}> {
       elem.setAttribute('aggregates', `
       {"stock": "distinct count",
       "top_ask_price":"avg",
-      "top_bid_price": "avg",
+      "top_bid_price":"avg",
       "timestamp":"distinct count"}`)
     }
   }
